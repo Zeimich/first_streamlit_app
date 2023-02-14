ae950364f474808c6cbb3b1db93f1d9e615a5c6c
Zeimich <125396625+Zeimich@users.noreply.github.com>
2023-02-14 19:46:10 +0100

    Update streamlit_app.py
    Corrección del menu del desayuno

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & blusberry')
-streamlit.text('Kale, spinach')
-streamlit.text('Hard-boiled free range-egg')
+streamlit.text('Omega 3 y avena con arándanos')
+streamlit.text('Batido de col rizada, espinacas y rúcula')
+streamlit.text('Huevo de gallinas camperas hervidas')
