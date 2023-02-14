a207feb6b32510c51b04dd0525e5cffd14061eb9
Zeimich <125396625+Zeimich@users.noreply.github.com>
2023-02-14 19:47:37 +0100

    Update streamlit_app.py
    Se añaden emoticonos al menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.text('🥣 Omega 3 y avena con arándanos')
+streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔 Huevo de gallinas camperas hervidas')
+streamlit.text ('🥑🍞 Tostada de aguacate')
+
