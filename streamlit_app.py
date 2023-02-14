4e8f2dfb2549d25e00e105fb194951aba6471068
Zeimich <125396625+Zeimich@users.noreply.github.com>
2023-02-14 19:52:09 +0100

    Update streamlit_app.py
    Se importa pandas y la lista de frutas desde el fichero csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,9 @@ streamlit.text('🐔 Huevo de gallinas camperas hervidas')
 streamlit.text ('🥑🍞 Tostada de aguacate')
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
