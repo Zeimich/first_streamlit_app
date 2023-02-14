fbc4975b1d7ba3f9645e8dbbd720fc24d7098bf7
Zeimich <125396625+Zeimich@users.noreply.github.com>
2023-02-14 19:45:06 +0100

    Update streamlit_app.py
    Se añade el menu del desayuno

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & blusberry')
+streamlit.text('Kale, spinach')
+streamlit.text('Hard-boiled free range-egg')
